4c4375d7a9ba09ae5146b31a76544c36e4efe7a4
stableapple <riddhi01.menon@gmail.com>
2021-01-19 17:09:18 +0530

    configured rtmp server

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef()
+
+    }
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id)
+        this.buildPlayer();
+
+    }
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+    buildPlayer() {
+        if(this.player || !this.props.stream) {
+            return;
+        }
+        this.player=flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${this.props.match.params.id}`
+        })
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load()
+
+
     }
     render() {
         if(!this.props.stream){
@@ -12,6 +40,7 @@ class StreamShow extends React.Component{
         }
         return(
             <div>
+                <video ref={this.videoRef} style={{ width:'50%'}} controls={true}/>
                 <h1>{this.props.stream.title}</h1>
                 <h5>{this.props.stream.description}</h5>
             </div>
