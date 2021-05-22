23c130ab64832b1ab7395efc19ceda5690040770
stableapple <riddhi01.menon@gmail.com>
2021-05-22 20:24:11 +0530

    removed error in flv

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -27,7 +27,7 @@ class StreamShow extends React.Component{
         }
         this.player=flv.createPlayer({
             type: 'flv',
-            url: `http://localhost:8000/live/${this.props.match.params.id}`
+            url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
         })
         this.player.attachMediaElement(this.videoRef.current);
         this.player.load()
